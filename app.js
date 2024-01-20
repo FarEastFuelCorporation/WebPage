0ff29e48f58c2fd63a3a5d3d98d446edc03091c0
jimfalar@gmail.com <jimfalar@gmail.com>
2024-01-20 18:06:54 +0800

    Update Files

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // app.js
 
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 
